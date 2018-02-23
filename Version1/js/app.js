42f6cb704189fbb6ac201ce364c2a66c725299fe
JinwooPark94 <wlsdntus4@gmail.com>
2018-02-24 00:27:12 +0900

    modify Todolist Version1

--- Version1/js/app.js
+++ Version1/js/app.js
@@ -1,7 +1,8 @@
-var todos = [ { id: 0, content : '123', completed : true },
-              { id: 1, content : 'qwer', completed: false },
-              { id: 2, content : 'zcxv', completed: true },
-              { id: 3, content : '1111', completed : true}];
+var todos = [ { id: 0, content : 'HTML', completed : true },
+              { id: 1, content : 'CSS', completed: false },
+              { id: 2, content : 'Javascript', completed: true },
+              { id: 3, content : 'JSON', completed : true},
+              { id: 4, content: 'Ajax', completed: false }];
 
 var todoUl = document.querySelector('#todo-list');
 var inputTodo = document.querySelector('#input-todo');
@@ -15,11 +16,10 @@ window.addEventListener('load', function () {
 });
 
 searchTodo.addEventListener('keyup', function (event) {
-    loadData('search');
+  loadData('search');
 });
 
 inputTodo.addEventListener('keyup', function (event) {
-  console.log(event.target.value);
   if (!event.target.value) return;
   if (event.keyCode === 13) {
     addTodo(event);
@@ -37,7 +37,7 @@ todoUl.addEventListener('click', function (event) {
     if (selectTodo.value === 'comple') {
       loadData('comple');
     } else if ( selectTodo.value === 'uncomple' ) {
-      (countComplete() === 0) ? loadData('comple') :loadData('uncomple');
+      loadData('uncomple');
     } else {
       loadData();
     }
@@ -53,6 +53,7 @@ selectTodo.addEventListener('click', function (event) {
   loadData(event.target.value);
 });
 
+// 전체 선택 및 전체 해제
 function checkAll(){
   if (countComplete() !== todos.length) {
     todos.forEach(function (item) {
@@ -65,6 +66,7 @@ function checkAll(){
   }
 }
 
+// 완료된 할일 수 반환
 function countComplete(){
   var completCount = 0;
 
@@ -75,11 +77,11 @@ function countComplete(){
   return completCount;
 }
 
-
+// 데이터 로드
 function loadData(filter){
+  var messageTodo = document.querySelector('#message-todo');
   var todoInner = '';
   var arrayName;
-  console.log(filter);
 
   // 완료 상태
   if (filter === 'comple') {
@@ -98,7 +100,7 @@ function loadData(filter){
     // 자동 검색
   } else if (filter === 'search') {
     arrayName = todos.filter( function (item){
-      return (item.content.match(searchTodo.value)) ? true : false;
+      return (item.content.toLowerCase().match(searchTodo.value.toLowerCase())) ? true : false;
     });
     if (!searchTodo.value) arrayName = todos;
 
@@ -107,6 +109,10 @@ function loadData(filter){
     selectTodo.value = 'all';
   }
 
+  console.log(arrayName.length);
+  (!arrayName.length) ? messageTodo.style.display = 'block' : messageTodo.style.display = 'none';
+
+  // 리스트 목록 출력
   arrayName.map(function (item) {
     var checkId = (item.completed) ? 'checked' : '';
 
@@ -117,7 +123,7 @@ function loadData(filter){
     todoInner += '</a>';
     todoInner += '<label class="i-checks" for="' + item.id +'">';
     todoInner += '<input type="checkbox" id="' + item.id + '"' + checkId +'><i></i>';
-    todoInner += '<span>' + item.content + '</span>';
+    todoInner += '<span class="' + checkId +'">' + item.content + '</span>';
     todoInner += '</label>';
     todoInner += '</div>';
     todoInner += '</li>';
@@ -127,18 +133,21 @@ function loadData(filter){
   completeAll.innerHTML = '완료된 할일 : ' + countComplete() + '개';
 }
 
+// ID값 얻기
 function getId(){
   return todos.length ? Math.max.apply(null, todos.map( function (item){
     return item.id;
   })) + 1 : 1;
 }
 
+// 할일 추가
 function addTodo(event){
   todos = todos.concat([{ id: getId(), content: event.target.value, completed: false }]);
   inputTodo.value = '';
   console.log('[[ADD DATA]] :', todos);
 }
 
+// 할일 삭제
 function deleteTodo(event){
   todos = todos.filter(function (item) {
     return item.id !== Number.parseInt(event.target.dataset.id);
@@ -146,6 +155,7 @@ function deleteTodo(event){
   console.log('[[DELETE COMPLETED]]', todos);
 }
 
+// 체크박스시 completed 값 반전시키기
 function chkChange(event){
   todos.forEach(function (item) {
     if (item.id === Number.parseInt(event.target.id)) item.completed = !item.completed;
